694bff8bbd8134e14329434fc96d364925d029fb
royambar1 <ambaroy1@gmail.com>
2022-05-04 15:50:23 +0300

    Ready for your check ofir

--- client.js
+++ client.js
@@ -27,60 +27,60 @@ class Day{
         "templateA.html",
         ["Time ","For ","Coffee \n","Elite "," Coffe"],
         ["Pics/EliteCoffe_ad2.jpg","Pics/coffe1_ad2.jpeg"],
-        [       new Day("suday",0,8,24),
-                new Day("monday",1,8,24),
-                new Day("tuesday",2,8,24),
-                new Day("wednesday",3,8,24),
-                new Day("thursday",4,8,24),],
-        [new Date(2022,1,3)],
-        [new Date(2022,5,31)],
-        10,
-        [1,3]
+        [
+                 new Day("monday",1,6,12),
+                 new Day("wednesday",3,6,12)
+        ],
+        [new Date(2022,1,1)],
+        [new Date(2022,12,31)],
+        3,
+        [2,3]
     ),
 
     new Ad("Ad2",
          "templateB.html",
          ["Learn ", "How ", "To ", "Code ","In ","Java","Script\n","Take a Free ","Course ","Now"],
          ["Pics/javascript.png"],
-         [      new Day("suday",0,8,24),
-                new Day("monday",1,8,24),
-                new Day("tuesday",2,8,24),
-                new Day("wednesday",3,8,24),
-                new Day("thursday",4,8,24),],
-         [new Date(2022,1,1)],
-         [new Date(2022,12,31)],
-         7,
-         [1,2]
+         [
+            new Day("tuesday",2,10,16),
+            new Day("wednesday",3,10,16)
+        ],
+         [new Date(2022,4,1)],
+         [new Date(2022,5,31)],
+         3,
+         [1]
     ),
 
      new Ad("Ad3",
          "templateC.html",
          [],
          [],
-         [      new Day("suday",0,8,24),
-                new Day("monday",1,8,24),
-                new Day("tuesday",2,8,24),
-                new Day("wednesday",3,8,24),
-                new Day("thursday",4,8,24),],
-         [new Date(2022,1,1)],
+         [
+            new Day("suday",0,8,22),
+            new Day("monday",1,8,22),
+            new Day("tuesday",2,8,22),
+            new Day("wednesday",3,8,22),
+            new Day("thursday",4,8,22),
+            new Day("friday",5,8,22),
+            new Day("saturday",6,8,22)
+        ],
+         [new Date(2022,5,1)],
          [new Date(2022,6,15)],
          3,
-         [2,3]
+         [3]
      ),
      new Ad("Ad4",
-         "templateA.html",
-         ["Line 1\n", "Line 2"],
-         [],
-         [      new Day("suday",0,8,24),
-                new Day("monday",1,8,24),
-                new Day("tuesday",2,8,24),
-                new Day("wednesday",3,8,24),
-                new Day("thursday",4,8,24),],
-         [new Date(2022,3,29)],
-         [new Date(2022,5,14)],
-         6,
-         [2]
-     ),
+     "templateA.html",
+     ["Roy Ambar\n", "Ofir Gur Cohen"],
+     [],
+     [
+        new Day("monday",1,15,19)
+    ],
+     [new Date(2022,3,29)],
+     [new Date(2022,4,15)],
+     3,
+     [1,2,3]
+ ),
      new Ad("Ad5",
          "templateB.html",
          ["iphone, ",
@@ -90,15 +90,15 @@ class Day{
                 "imouse, ",
                 "ikeyboard."],
          ["Pics/appleStore_ad5.jpeg","Pics/apple1_ad5.png"],
-         [      new Day("suday",0,8,24),
-                new Day("monday",1,8,24),
-                new Day("tuesday",2,8,24),
-                new Day("wednesday",3,8,24),
-                new Day("thursday",4,8,24),],
-         [new Date(2022,4,1)],
-         [new Date(2022,4,31)],
-         9,
-         [3]
+         [
+            new Day("monday",1,1,23),
+            new Day("tuesday",2,1,23),
+            new Day("wednesday",3,1,23)
+        ],
+         [new Date(2022,5,1)],
+         [new Date(2022,5,30)],
+         3,
+         [1,2,3]
      )
  ];
 
