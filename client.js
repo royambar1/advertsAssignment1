1fe678bd47a520cbd5aa3e038d95cbe2aca561d2
royambar1 <ambaroy1@gmail.com>
2022-05-04 13:14:54 +0300

    RoyChanges
    Changed a bit of the code and mainly the design and CSS

--- client.js
+++ client.js
@@ -21,83 +21,80 @@ class Day{
     }
 }
 
-// The ads array argument
+// Array of all ads
  var ads = [
-     new Ad("meridian",
-         "templateA.html",
-         ["Waterproof", "Shock resistant", "Cordless", "90 Minutes Trim Time Fully Charged"],
-         ["pics/Meridian_balls.jpg", "pics/Meridian_case.jpg"],
-         [ new Day("monday",1,6,12),
-           new Day("friday",5,8,22),
-           new Day("wednesday",3,13,20),
-           new Day("saturday",6,8,22)],
+    new Ad("Ad1",
+        "templateA.html",
+        ["Time ","For ","Coffee \n","Elite "," Coffe"],
+        ["Pics/EliteCoffe_ad2.jpg","Pics/coffe1_ad2.jpeg"],
+        [       new Day("suday",0,8,24),
+                new Day("monday",1,8,24),
+                new Day("tuesday",2,8,24),
+                new Day("wednesday",3,8,24),
+                new Day("thursday",4,8,24),],
+        [new Date(2022,1,3)],
+        [new Date(2022,5,31)],
+        10,
+        [1,3]
+    ),
+
+    new Ad("Ad2",
+         "templateB.html",
+         ["Learn ", "How ", "To ", "Code ","In ","Java","Script\n","Take a Free ","Course ","Now"],
+         ["Pics/javascript.png"],
+         [      new Day("suday",0,8,24),
+                new Day("monday",1,8,24),
+                new Day("tuesday",2,8,24),
+                new Day("wednesday",3,8,24),
+                new Day("thursday",4,8,24),],
          [new Date(2022,1,1)],
          [new Date(2022,12,31)],
          7,
          [1,2]
-     ),
-     new Ad("Mcdonalds",
-        "./templateB.html",
-        ["Big mac ............................................ 34nis",
-               "Mac Royal......................................... 42nis",
-               "Big America...................................... 48nis",
-               "Big Tokyo.......................................... 48nis",
-               "Big Japna.......................................... 48nis",
-               "Big Mombai....................................... 60nis",
-               "Big Texas............................................ 48nis",
-               "McNuggets........................................ 30nis",
-               "Potuto&chips..................................... 25nis",
-               "Happy Meal....................................... 34nis"],
-        ["pics/Mac.jpg"],
-        [       new Day("tuesday",2,10,16),
-                new Day("friday",5,8,22),
-                new Day("wednesday",3,10,16),
-                new Day("saturday",6,8,22)],
-        [new Date(2022,1,3)],
-        [new Date(2022,4,31)],
-        10,
-        [1,3]
     ),
-     new Ad("Trivago",
+
+     new Ad("Ad3",
          "templateC.html",
          [],
          [],
-         [new Day("sunday",0,8,22),
-             new Day("monday",1,8,22),
-             new Day("tuesday",2,8,22),
-             new Day("wednesday",3,8,22),
-             new Day("thursday",4,8,22),
-             new Day("friday",5,8,22),
-             new Day("saturday",6,8,22)],
+         [      new Day("suday",0,8,24),
+                new Day("monday",1,8,24),
+                new Day("tuesday",2,8,24),
+                new Day("wednesday",3,8,24),
+                new Day("thursday",4,8,24),],
          [new Date(2022,1,1)],
          [new Date(2022,6,15)],
          3,
          [2,3]
      ),
-     new Ad("Livnat Poran",
+     new Ad("Ad4",
          "templateA.html",
-         ["Halita? niftsata?", "we are here for you!"],
+         ["Line 1\n", "Line 2"],
          [],
-         [new Day("monday",1,15,19),
-         new Day("saturday",6,8,22)],
+         [      new Day("suday",0,8,24),
+                new Day("monday",1,8,24),
+                new Day("tuesday",2,8,24),
+                new Day("wednesday",3,8,24),
+                new Day("thursday",4,8,24),],
          [new Date(2022,3,29)],
          [new Date(2022,5,14)],
          6,
          [2]
      ),
-     new Ad("Durex",
+     new Ad("Ad5",
          "templateB.html",
-         ["Don't forget to wear your mask",
-                "Don't limit yourself with treats",
-                "freedom means responsibility",
-                "Toys change, playtime Doesn't",
-                "Protect yourself and your love ones",
-                "Get in you closest drag store"],
-         ["./pics/sand.jpg","./pics/ps5.jpg"],
-         [new Day("monday",1,1,23),
-             new Day("tuesday",2,1,23),
-             new Day("wednesday",3,1,23),
-             new Day("saturday",6,8,22)],
+         ["iphone, ",
+                "ipod, ",
+                "ipad, ",
+                "imac, ",
+                "imouse, ",
+                "ikeyboard."],
+         ["Pics/appleStore_ad5.jpeg","Pics/apple1_ad5.png"],
+         [      new Day("suday",0,8,24),
+                new Day("monday",1,8,24),
+                new Day("tuesday",2,8,24),
+                new Day("wednesday",3,8,24),
+                new Day("thursday",4,8,24),],
          [new Date(2022,4,1)],
          [new Date(2022,4,31)],
          9,
@@ -105,42 +102,39 @@ class Day{
      )
  ];
 
+ //Array of current Time
 function Currenttime(){
-    var date_now = new Date();
-    var dayInAWeek = date_now.getDay();
-    var dateInMonth = date_now.getDate();
-    var month = date_now.getMonth();
-    var year = date_now.getFullYear();
-    var hour = date_now.getHours();
+    var dateNow = new Date();
+    var dayInAWeek = dateNow.getDay();
+    var dateInMonth = dateNow.getDate();
+    var month = dateNow.getMonth();
+    var year = dateNow.getFullYear();
+    var hour = dateNow.getHours();
     return [hour,dayInAWeek,dateInMonth,month,year];
 }
 
+//Collects all relevant ads to run now into an array
 function adSelector(){
 
     let adsArray = [];
     var time = Currenttime();
     for(i = 0; i<ads.length;i++)
     {
+        //if year is current year
         if (ads[i].fromDate[0].getFullYear() <= time[4] || ads[i].toDate[0].getFullYear() >= time[4])
         {
-            console.log('if 1 #' + i);
+            //if month is current month
             if (ads[i].fromDate[0].getDate() <= time[3] || ads[i].toDate[0].getDate() >= time[3])
             {
-                console.log('if 2 #' + i)
+                //if day is current day
                 if(ads[i].fromDate[0].getDay() <= time[2] || ads[i].toDate[0].getDay() >= time[2])
                 {
+                    //runs on all days
                     for (j = 0; j<ads[i].days.length;j++)
                     {
-                        console.log('if 3 #' + i)
-                        if (ads[i].days[j].daynumber == time[1])
-                        {
-                          console.log(ads[i].days[j].daynumber)
-                          console.log(time[1])
-                        }
-
+                        //checks for days and time frame
                         if ((ads[i].days[j].daynumber == time[1]) && (ads[i].days[j].fromHour <= time[0]) && (ads[i].days[j].toHour >= time[0]) )
                         {
-                            console.log('if 4 #' + i)
                             adsArray.push(ads[i]);
                         }
                     }
@@ -152,41 +146,40 @@ function adSelector(){
     localStorage.ads = JSON.stringify(adsArray);
 }
 
-function adsPublisher(){
-    let ads_array = JSON.parse(localStorage.ads);
-    var single_ad = ads_array[0];
-    ads_array.shift();
-    localStorage.ads = JSON.stringify(ads_array);
-    printHead(single_ad);
-    printText(single_ad);
-    timeDelay = setTimeout(swift,single_ad["timeDuration"]*1000);
+function adsDisplay(){
+    let adsArray = JSON.parse(localStorage.ads);
+    var singleAd = adsArray[0];
+    adsArray.shift();
+    localStorage.ads = JSON.stringify(adsArray);
+    printText(singleAd);
+    printImg(singleAd);
+    timeDelay = setTimeout(swift,singleAd.timeDuration*1000);
 }
 
-function printHead(current_ad){
-    document.write('<header id="head">' + current_ad["name"] + "(based on " + current_ad["templateUrl"] + ")<br/>" + '</header>');
+function printText(currentAd){
+    let display = document.getElementById("grid-item1");
+    display.innerText = currentAd.texts.join('');
 }
 
-function printText(current_ad){
-    for(let elmnt of ads){
-        if(!elmnt["name"].localeCompare(current_ad["name"])){
-            var template = current_ad["templateUrl"]
-            for(let text of elmnt["texts"]){
-                document.write('<body id="body">' + text + "<br/>" + '</body>');
-            }
-            for(let img of elmnt["imageUrl"])
-            printImg(img);
+function printImg(currentAd){
+    for(let imgArr of ads){
+        if(!imgArr.name.localeCompare(currentAd.name)){
+            if(imgArr.imageUrl.length!=0)    
+                for(let img of imgArr.imageUrl)
+                printImgDimensions(img);
         }
     }
 }
 
-function printImg(source){
+function printImgDimensions(source){
     var img = document.createElement("img");
     img.src = source;
-    img.style.height = '300px';
-    img.style.width = '600px';
+    img.style.alignContent = 'bottom';
+    img.style.height = 'auto';
+    img.style.width = '200px';
     img.style.marginBlock = '20px';
-    var block = document.getElementById("x");
-    block.appendChild(img);
+    let display = document.getElementById("grid-item2");
+    display.appendChild(img);
 }
 
 function templateSelector(){
